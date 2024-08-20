3ab33a18a78d11c659defa220b5304fc86dfae3b
Naveen Kumar K <55081554+snaveen1856@users.noreply.github.com>
2024-08-20 10:08:43 +0000

    Initial commit for JS and PY.:

---
+++ JS_learnings/calculator_pro/index.js
@@ -0,0 +1,19 @@
+// Calculate program.
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    } catch (err) {
+        display.value = "Error";
+    }
+}
